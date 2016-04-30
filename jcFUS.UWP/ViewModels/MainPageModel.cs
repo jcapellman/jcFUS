759769e98d70e10650d62c8a76a81e1c57ae54dd
Jarred Capellman <jcapellman@hotmail.com>
2016-04-30 09:41:02 -0400

    Added Person Support in the VM

--- jcFUS.UWP/ViewModels/MainPageModel.cs
+++ jcFUS.UWP/ViewModels/MainPageModel.cs
@@ -2,7 +2,11 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using Windows.Media;
+using Windows.Media.Audio;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Media.Render;
 using jcFUS.PCL.Transports;
 
 namespace jcFUS.UWP.ViewModels {
@@ -14,8 +18,23 @@ namespace jcFUS.UWP.ViewModels {
             set { _chatlog = value; OnPropertyChanged(); }
         }
 
+        private ObservableCollection<PersonItem> _connectedPeople;
+
+        public ObservableCollection<PersonItem> ConnectedPeople {
+            get { return _connectedPeople; }
+            set { _connectedPeople = value; OnPropertyChanged(); }
+        }  
+
         public MainPageModel() {
             ChatLog = new ObservableCollection<ChatLogItem>();
+
+            ConnectedPeople = new ObservableCollection<PersonItem>();
+
+            ConnectedPeople.Add(new PersonItem {
+                Color = "Green",
+                GUID = Guid.NewGuid(),
+                Name = "Jarred"
+            });
         }
 
         private string _chatEntry;
@@ -39,6 +58,35 @@ namespace jcFUS.UWP.ViewModels {
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void EnableMicrophone()
+        {
+            AudioGraphSettings settings = new AudioGraphSettings(AudioRenderCategory.Communications);
+        
+            settings.DesiredRenderDeviceAudioProcessing = Windows.Media.AudioProcessing.Default;
+            settings.QuantumSizeSelectionMode = QuantumSizeSelectionMode.LowestLatency;
+
+
+            var result = await AudioGraph.CreateAsync(settings);
+
+            var audioGraph = result.Graph;
+
+            await audioGraph.CreateDeviceInputNodeAsync(MediaCategory.Communications,
+                AudioEncodingProperties.CreateMp3(44100, 2, 128));
+
+            frameOutputNode = audioGraph.CreateFrameOutputNode();
+
+            audioGraph.QuantumProcessed += AudioGraphy_QuantumProcessed;
+
+            audioGraph.Start();
+           
+        }
+
+        private AudioFrameOutputNode frameOutputNode;
+
+        private void AudioGraphy_QuantumProcessed(AudioGraph sender, object args) {
+            AudioFrame frame = frameOutputNode.GetFrame();
         }
     }
 }
