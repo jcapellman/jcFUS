2a1bc1a9ff3bf99a4cb0f24cb66989b6729e7ac1
Jarred Capellman <jcapellman@hotmail.com>
2016-04-30 09:41:30 -0400

    Added PersonItem Transport

---
+++ jcFUS.PCL/Transports/PersonItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace jcFUS.PCL.Transports {
+    [DataContract]
+    public class PersonItem {
+        [DataMember]
+        public Guid GUID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+    }
+}
