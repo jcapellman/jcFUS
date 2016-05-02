f4b6fd5d31738147e853c7b1587e2a346f51349e
Jarred Capellman <jcapellman@hotmail.com>
2016-05-01 21:42:16 -0400

    Added Login support

---
+++ jcFUS.UWP/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using jcFUS.UWP.ViewModels;
+
+namespace jcFUS.UWP {
+    public sealed partial class LoginPage : Page {
+        private LoginPageModel viewModel => (LoginPageModel) DataContext;
+
+        public LoginPage() {
+            this.InitializeComponent();
+
+            DataContext = new LoginPageModel();
+        }
+
+        private async void btnLogin_Clicked(object sender, RoutedEventArgs e) {
+            var result = await viewModel.AttemptLogin();
+
+            if (result == null) {
+                await new MessageDialog("Invalid username or password").ShowAsync();
+
+                return;
+            }
+
+            App.CURRENT_USER = result;
+
+            Frame.Navigate(typeof (MainPage));
+        }
+    }
+}
---
+++ jcFUS.UWP/ViewModels/LoginPageModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using jcFUS.PCL.Handlers;
+using jcFUS.PCL.Transports.Auth;
+
+namespace jcFUS.UWP.ViewModels {
+    public class LoginPageModel : INotifyPropertyChanged {
+        private string _emailAddress;
+
+        public string EmailAddress {
+            get { return _emailAddress; }
+            set { _emailAddress = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+
+        public string Password {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public async Task<AuthResponseItem> AttemptLogin() {
+            var authHandler = new AuthHandler();
+
+            var result = await authHandler.CheckAuthAsync(new AuthRequestItem {
+                Username = EmailAddress,
+                Password = this.Password
+            });
+
+            return result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
