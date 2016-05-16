c153239ca068ccf63198d2f202cb0954d17ba189
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:13:10 -0700

    Added additional DbSets in the EFModel

--- jcFUS.WebAPI.DataLayerLibrary/Entities/EFModel.cs
+++ jcFUS.WebAPI.DataLayerLibrary/Entities/EFModel.cs
@@ -1,7 +1,38 @@
+using System;
 using System.Data.Entity;
+using jcFUS.WebAPI.DataLayerLibrary.EFObjects;
 
 namespace jcFUS.WebAPI.DataLayerLibrary.Entities {    
     public class EFModel : DbContext {
+        public DbSet<Locations> LocationSet { get; set; }
+
+        public DbSet<Users> UserSet { get; set; }
+
+        public DbSet<Room> RoomSet { get; set; }
+
+        public DbSet<Users2Locations> Users2LocationSet { get; set; }
+
         public EFModel() : base("name=EFModel") { }
+
+        public override int SaveChanges() {
+            foreach (var item in this.ChangeTracker.Entries()) {
+                if (item.State == EntityState.Deleted || item.State == EntityState.Modified ||
+                    item.State == EntityState.Added) {
+                    item.Member("Modified").CurrentValue = DateTimeOffset.Now;
+                }
+
+                switch (item.State) {
+                    case EntityState.Deleted:
+                        item.Member("Active").CurrentValue = false;
+                        break;
+                    case EntityState.Added:
+                        item.Member("Created").CurrentValue = DateTimeOffset.Now;
+                        item.Member("Active").CurrentValue = true;
+                        break;
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
