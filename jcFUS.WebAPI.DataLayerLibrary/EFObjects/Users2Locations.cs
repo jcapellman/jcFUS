cfe9fdfc58901141e4e752b8a0b9c8c296cca3ad
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:16:55 -0700

    Added: Users2Locations EF Class Object

---
+++ jcFUS.WebAPI.DataLayerLibrary/EFObjects/Users2Locations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace jcFUS.WebAPI.DataLayerLibrary.EFObjects {
+    public class Users2Locations : BaseObject {
+        public Guid UserGUID { get; set; }
+
+        public Guid LocationGUID { get; set; }
+    }
+}
