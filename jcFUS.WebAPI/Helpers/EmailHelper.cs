3e1f6afd80b5f46841da38782761182666dfbc8a
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:17:40 -0700

    Added: EmailHelper Class

---
+++ jcFUS.WebAPI/Helpers/EmailHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace jcFUS.WebAPI.Helpers {
+    public static class EmailHelper {
+        public static void SendEmail(string receiver, string subject, string body)
+            => SendEmail(new List<string> { receiver }, subject, body);
+
+        public static void SendEmail(List<string> receivers, string subject, string body) {
+            using (var smtpClient = new SmtpClient()) {
+                var mm = new MailMessage();
+
+                mm.To.Add(String.Join(",", receivers));
+
+                mm.Subject = subject;
+                mm.Body = body;
+                
+                smtpClient.SendAsync(mm, null);
+            }
+        }
+    }
+}
