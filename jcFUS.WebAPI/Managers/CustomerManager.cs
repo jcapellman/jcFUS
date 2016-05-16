df433c13cf51e7b3f7237a0d77607792e61ba182
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:18:02 -0700

    Added: CustomerManager Class in the WebAPI Service

---
+++ jcFUS.WebAPI/Managers/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using jcFUS.PCL.Transports.Customer;
+
+namespace jcFUS.WebAPI.Managers {
+    public class CustomerManager : BaseManager {
+        public bool Create(CustomerCreationRequestItem requestItem) {
+            var locationGUID = new LocationManager().Create(requestItem.LocationName);
+
+            if (!locationGUID.HasValue) {
+                return false;
+            }
+
+            var roomGUID = new RoomManager().Create(requestItem.InitialRoomName);
+
+            if (!roomGUID.HasValue) {
+                return false;
+            }
+
+            requestItem.UserCreationRequestItem.HomeRoomGUID = roomGUID.Value;
+            requestItem.UserCreationRequestItem.LocationGUID = locationGUID.Value;
+
+            var userGUID = new UserManager().Create(requestItem.UserCreationRequestItem);
+
+            return userGUID.HasValue;
+        }
+    }
+}
