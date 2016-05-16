e79ce1d7938cbdf36f3aab07a5b07df06de47978
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:14:21 -0700

    Added: UserCreationRequestItem Class

---
+++ jcFUS.PCL/Transports/Users/UserCreationRequestItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace jcFUS.PCL.Transports.Users {
+    public class UserCreationRequestItem {
+        public string DisplayName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public Guid Password { get; set; }
+
+        public string ProfileColor { get; set; }
+
+        public Guid HomeRoomGUID { get; set; }
+
+        public Guid LocationGUID { get; set; }
+    }
+}
