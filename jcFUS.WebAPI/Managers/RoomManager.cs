b17ec76254d4ce21ac1fbb1cc77bde1b81ff32ae
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:19:14 -0700

    Added: RoomManager Class in the WebAPI Service

---
+++ jcFUS.WebAPI/Managers/RoomManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using jcFUS.WebAPI.DataLayerLibrary.Entities;
+
+namespace jcFUS.WebAPI.Managers {
+    public class RoomManager : BaseManager {
+        public Guid? Create(string name) {
+            using (var eFactory = new EFModel()) {
+                var room = eFactory.RoomSet.Create();
+
+                room.Description = name;
+
+                eFactory.RoomSet.Add(room);
+
+                var result = eFactory.SaveChanges();
+
+                return result > 0 ? (Guid?)room.GUID : null;
+            }
+        }
+    }
+}
