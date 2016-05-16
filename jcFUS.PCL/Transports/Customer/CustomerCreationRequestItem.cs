231f01bf0750092086ff42a6f689798433120fb4
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:14:00 -0700

    Added: CustomerCreationRequestItem Class

---
+++ jcFUS.PCL/Transports/Customer/CustomerCreationRequestItem.cs
@@ -0,0 +1,11 @@
+﻿using jcFUS.PCL.Transports.Users;
+
+namespace jcFUS.PCL.Transports.Customer {
+    public class CustomerCreationRequestItem {
+        public string LocationName { get; set; }
+
+        public string InitialRoomName { get; set; }
+
+        public UserCreationRequestItem UserCreationRequestItem { get; set; }
+    }
+}
