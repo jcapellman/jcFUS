2531794846e717aa42b451df9b8ef4801c022ed7
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:17:23 -0700

    Added: CustomerController to the WebAPI Service

---
+++ jcFUS.WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,8 @@
+﻿using jcFUS.PCL.Transports.Customer;
+using jcFUS.WebAPI.Managers;
+
+namespace jcFUS.WebAPI.Controllers {
+    public class CustomerController : BaseController {
+        public bool POST(CustomerCreationRequestItem requestItem) => new CustomerManager().Create(requestItem);
+    }
+}
