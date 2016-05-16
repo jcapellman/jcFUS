58a7c32ad574c09f9743eef6425a8b2a665a0abc
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 15:18:48 -0700

    Added: LocationManager in the WebAPI Service

---
+++ jcFUS.WebAPI/Managers/LocationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using jcFUS.WebAPI.DataLayerLibrary.Entities;
+
+namespace jcFUS.WebAPI.Managers {
+    public class LocationManager : BaseManager {
+        public Guid? Create(string description) {
+            using (var eFactory = new EFModel()) {
+                var item = eFactory.LocationSet.Create();
+
+                item.Description = description;
+
+                eFactory.LocationSet.Add(item);
+
+                var result = eFactory.SaveChanges();
+
+                if (result > 0) {
+                    return item.GUID;
+                }
+
+                return null;
+            }
+        }
+
+        public Guid? AddUser(Guid locationGUID, Guid userGUID) {
+            using (var eFactory = new EFModel()) {
+                var locationRelation = eFactory.Users2LocationSet.Create();
+
+                locationRelation.LocationGUID = locationGUID;
+                locationRelation.UserGUID = userGUID;
+
+                eFactory.Users2LocationSet.Add(locationRelation);
+
+                var result = eFactory.SaveChanges();
+
+                return result >= 0 ? (Guid?)locationRelation.GUID : null;
+            }
+        }
+    }
+}
