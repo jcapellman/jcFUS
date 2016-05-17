995563a928f4a921c4eb4164369c9de0fd4c3206
Jarred Capellman <jcapellman@hotmail.com>
2016-05-16 23:12:07 -0400

    Added: EmailContent EF Class

---
+++ jcFUS.WebAPI.DataLayerLibrary/EFObjects/Tables/EmailContent.cs
@@ -0,0 +1,9 @@
+﻿namespace jcFUS.WebAPI.DataLayerLibrary.EFObjects.Tables {
+    public class EmailContent : BaseTableObject {
+        public int EmailTypeID { get; set; }
+
+        public string SubjectLine { get; set; }
+
+        public string Body { get; set; }
+    }
+}
