d766e9d9f0c3f92fa2cf4567949bcf0a44306ddd
Jarred Capellman <jcapellman@hotmail.com>
2016-05-22 19:19:37 -0400

    Updated the CustomerController and CustomerManager to utilize the ReturnSet container

--- jcFUS.WebAPI/Controllers/CustomerController.cs
+++ jcFUS.WebAPI/Controllers/CustomerController.cs
@@ -1,8 +1,9 @@
 ﻿using jcFUS.PCL.Transports.Customer;
+using jcFUS.PCL.Transports.Global;
 using jcFUS.WebAPI.Managers;
 
 namespace jcFUS.WebAPI.Controllers {
     public class CustomerController : BaseController {
-        public bool POST(CustomerCreationRequestItem requestItem) => new CustomerManager().Create(requestItem);
+        public ReturnSet<bool> POST(CustomerCreationRequestItem requestItem) => new CustomerManager().Create(requestItem);
     }
 }
--- jcFUS.WebAPI/Managers/CustomerManager.cs
+++ jcFUS.WebAPI/Managers/CustomerManager.cs
@@ -1,18 +1,19 @@
 ﻿using jcFUS.PCL.Transports.Customer;
+using jcFUS.PCL.Transports.Global;
 
 namespace jcFUS.WebAPI.Managers {
     public class CustomerManager : BaseManager {
-        public bool Create(CustomerCreationRequestItem requestItem) {
+        public ReturnSet<bool> Create(CustomerCreationRequestItem requestItem) {
             var locationGUID = new LocationManager().Create(requestItem.LocationName);
 
             if (!locationGUID.HasValue) {
-                return false;
+                return new ReturnSet<bool>(false);
             }
 
             var roomGUID = new RoomManager().Create(requestItem.InitialRoomName);
 
             if (!roomGUID.HasValue) {
-                return false;
+                return new ReturnSet<bool>(false);
             }
 
             requestItem.UserCreationRequestItem.HomeRoomGUID = roomGUID.Value;
@@ -20,7 +21,7 @@ namespace jcFUS.WebAPI.Managers {
 
             var userGUID = new UserManager().Create(requestItem.UserCreationRequestItem);
 
-            return userGUID.HasValue;
+            return new ReturnSet<bool>(userGUID.HasValue);
         }
     }
 }
