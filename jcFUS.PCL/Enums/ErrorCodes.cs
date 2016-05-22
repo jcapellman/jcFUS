151dfb939ab75dfb3bcec971ad232f0a841a85a4
Jarred Capellman <jcapellman@hotmail.com>
2016-05-22 19:20:08 -0400

    Added ErrorCodes Enumeration

---
+++ jcFUS.PCL/Enums/ErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace jcFUS.PCL.Enums {
+    public enum ErrorCodes {
+        NONE,
+        UNCATEGORIZED,
+        REGISTRATION_EMAIL_ALREADY_IN_USE,
+        LOGIN_FAILED_BAD_AUTH
+    }
+}
