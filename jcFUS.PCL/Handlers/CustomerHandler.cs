3c9a4d118b9433abf8adefcd8ed02caf8176bd48
Jarred Capellman <jcapellman@hotmail.com>
2016-05-22 19:21:06 -0400

    Added CustomerHandler WebAPI Wrapper class

---
+++ jcFUS.PCL/Handlers/CustomerHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+using jcFUS.PCL.Transports.Customer;
+using jcFUS.PCL.Transports.Global;
+
+namespace jcFUS.PCL.Handlers {
+    public class CustomerHandler : BaseHandler {
+        protected override string BaseControllerName() => "Customer";
+
+        public async Task<ReturnSet<bool>> CreateCustomer(CustomerCreationRequestItem requestItem)
+            => await PostAsync<CustomerCreationRequestItem, ReturnSet<bool>>(requestItem);
+    }
+}
